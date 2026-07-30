85ae54b7ae8f6002ab6f23217089e2529ce78572
YASH WASULKAR <145082484+yashkun00@users.noreply.github.com>
2026-07-30 18:53:47 +0530

    a

--- basic.py
+++ basic.py
@@ -1,35 +1,43 @@
-print("hello world")
-------x-----x-----
-a = num
-b = num
-return a + b
 
+
+print("hello world")
+
+
+# ------x-----x-----
 a = 1
 b = 2
-print("result =  ")
-#(result = 3)
--------x------x---
-c = num
-d = num 
-return a-b
+
+result = a + b
+print("Result =", result)
+
+# -----x--------x-------
 
 c = 3
 d = 1
-print("result =  ")
-#(result = 2)
+
+result = c - d
+print("Result =", result)
+
+# ------x-----x-----
+
 
 e = 5
 f = 1
-multi_result = e*f
 
-# Print the result
-print("The sum of", a, "and", b, "is:", sum_result)
-print("The sub of", c, "and", d, "is:", sub_result)
-print("The multi of", e, "and", f, "is:", multi_result)
+result = e*f
+
+
+
+print("Result: ", result)
+
+
+
+# ------x-----x---
 
 
 x=12 
 y=2
+
 divide_result  = x/y 
 
 print("The divide of", x, "and", y, "is:", divide_result)
